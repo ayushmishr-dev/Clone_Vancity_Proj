083d89cc0ba696c2daa704ca5c667a8abf34765a
Ayush Mishra <ayushmishr.dev@gmail.com>
2025-12-25 20:34:50 +0530

    Initial upload

---
+++ assets/js/jquery-migrate-3.1.0.min.js
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="For overseas education, the most popular “DEF” rule will motivate you to turn your dream into reality. i.e. Dream of studying abroad, endeavor for it (work hard to achieve it) and fly high towards success.">
+    <meta name="tags" content="Studying Abroad : Dream | Endeavour | Fly">
+
+    <title>Studying Abroad : Dream | Endeavour | Fly - Immigration &amp; Visa Consultation Service</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:500" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Titillium+Web:700,900" rel="stylesheet">
+    <link rel="icon" href="https://vancityglobal.com/assets/uploads/media-uploader/asset-2-at-4x-91659185698.png" type="image/png">
+        <link rel="stylesheet" href="https://vancityglobal.com/assets/frontend/css/jquery.ihavecookies.css">
+    <style>
+        :root {
+            --main-color-one: #ff8a73;
+            --secondary-color: #5580ff;
+            --heading-color: #0a1121;
+            --paragraph-color: #878a95;
+        }
+    </style>
+    <style>
+        * {
+            -webkit-box-sizing: border-box;
+            box-sizing: border-box;
+        }
+
+        body {
+            padding: 0;
+            margin: 0;
+        }
+
+        #notfound {
+            position: relative;
+            height: 100vh;
+        }
+
+        #notfound .notfound {
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            -webkit-transform: translate(-50%, -50%);
+            -ms-transform: translate(-50%, -50%);
+            transform: translate(-50%, -50%);
+        }
+
+        .notfound {
+            max-width: 767px;
+            width: 100%;
+            line-height: 1.4;
+            padding: 0px 15px;
+        }
+
+        .notfound .notfound-404 {
+            position: relative;
+            height: 150px;
+            line-height: 150px;
+            margin-bottom: 25px;
+        }
+
+        .notfound .notfound-404 h1 {
+            font-family: 'Titillium Web', sans-serif;
+            font-size: 186px;
+            font-weight: 900;
+            margin: 0px;
+            text-transform: uppercase;
+            color: var(--main-color-one);
+        }
+
+        .notfound h2 {
+            font-family: 'Titillium Web', sans-serif;
+            font-size: 26px;
+            font-weight: 700;
+            margin: 0;
+            color: var(--heading-color);
+        }
+
+        .notfound p {
+            font-family: 'Montserrat', sans-serif;
+            font-size: 14px;
+            font-weight: 500;
+            margin-bottom: 0px;
+            color: var(--paragraph-color);
+        }
+
+        .notfound a {
+            font-family: 'Titillium Web', sans-serif;
+            display: inline-block;
+            text-transform: uppercase;
+            color: #fff;
+            text-decoration: none;
+            border: none;
+            background: var(--secondary-color);
+            padding: 10px 40px;
+            font-size: 14px;
+            font-weight: 700;
+            border-radius: 1px;
+            margin-top: 15px;
+            -webkit-transition: 0.2s all;
+            transition: 0.2s all;
+        }
+
+        .notfound a:hover {
+            background: var(--main-color-one);
+        }
+
+        @media  only screen and (max-width: 767px) {
+            .notfound .notfound-404 {
+                height: 110px;
+                line-height: 110px;
+            }
+            .notfound .notfound-404 h1 {
+                font-size: 120px;
+            }
+        }
+    </style>
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+    <body>
+        <div id="notfound">
+            <div class="notfound">
+                <div class="notfound-404">
+                    <h1>404</h1>
+                </div>
+                <h2>Oops! This Page Could Not Be Found</h2>
+                <p>Sorry but the page you are looking for does not exist, have been removed. name changed or is temporarily unavailable</p>
+                <a href="https://vancityglobal.com">Go To Home Page</a>
+            </div>
+        </div>
+    </body>
+</html>
